e0169e6ebcf27af650f607fdc50845244de3f2da
YUVA TEJA PANDRANKI <42350361+YuvaTeja-Pandranki@users.noreply.github.com>
2023-10-15 16:26:58 -0500

    Update PasswordCracker.py

--- PasswordCracker.py
+++ PasswordCracker.py
@@ -1,6 +1,7 @@
 from random import *
 import os
-u_pwd = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+u_pwd = input("Enter a password: ")
+pwd = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
 
 pw = ""
 while(pw != u_pwd):
